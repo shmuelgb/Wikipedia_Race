4729b767d1dae2301150a9a30a6c56e857a5e329
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2022-01-01 23:37:47 +0200

    setup switch case for routing

--- src/Components/WaitingRoom/WaitingRoom.jsx
+++ src/Components/WaitingRoom/WaitingRoom.jsx
@@ -1,38 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-import { useSessionStatusPro } from "../../Provider/Session_provider";
 
 export default function WaitingRoom() {
-  const [sessionStatus, setSessionStatus] = useSessionStatusPro();
-
-  //   useEffect(() => {
-  //     if (sessionStatus !== "waiting") {
-  //       history.push("/game");
-  //     }
-  //   }, [sessionStatus]);
-
-  const startGame = () => {
-    setSessionStatus("active");
-  };
-
-  const showContinueButton = () => {
-    if (sessionStatus !== "waiting") {
-      return (
-        <Link onClick={() => startGame()} to="/game">
-          Continue
-        </Link>
-      );
-    }
-  };
-
   return (
     <div>
       WaitingRoom
       <br />
       ***Game Explanation***
-      <br />
-      {showContinueButton()}
     </div>
   );
 }
